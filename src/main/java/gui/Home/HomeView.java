931ee375e2b2dc93579421cb91a111c3f75a0dd6
gandalfthegloom <farrellarifandipurbasidadolog@gmail.com>
2025-11-15 08:16:12 -0500

    Completed HomeViewModel.java based on newest Habit classes

--- src/main/java/gui/Home/HomeView.java
+++ src/main/java/gui/Home/HomeView.java
@@ -1,4 +1,25 @@
-package gui;
+package gui.Home;
+
+import javax.swing.*;
 
 public class HomeView {
+    private HomeViewModel homeViewModel;
+    private JFrame frame;
+
+
+    private JPanel MainPanel;
+    private JPanel ModalPanel; /// Day, Week, Month
+    private JPanel SettingsPanel;
+    private JPanel TasksPanel;
+    private JPanel ButtonsPanel;
+
+    public HomeView(HomeViewModel homeViewModel) {
+        // Assign viewmodel
+        this.homeViewModel = homeViewModel;
+        frame = new JFrame("Home");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void createUIComponents() {}
+
 }
--- src/main/java/gui/Home/HomeViewModel.java
+++ src/main/java/gui/Home/HomeViewModel.java
@@ -1,4 +1,26 @@
 package gui.Home;
 
+import main.Habit;
+import placeholders.PlaceholderTask;
+
+import java.util.ArrayList;
+
 public class HomeViewModel {
+    /// Assume ViewModel accepts arrays of tasks
+
+    public ArrayList<Habit> dailyHabits;
+    public ArrayList<Habit> weeklyHabits;
+    public ArrayList<Habit> monthlyHabits;
+
+    public HomeViewModel(ArrayList<Habit> habits) {
+        for (Habit habit : habits) {
+            if (habit.freq == Habit.Freq.Every_day) {
+                this.dailyHabits.add(habit);
+            } else if (habit.freq == Habit.Freq.Every_week) {
+                this.weeklyHabits.add(habit);
+            } else if (habit.freq == Habit.Freq.Every_month) {
+                this.monthlyHabits.add(habit);
+            }
+        }
+    }
 }
