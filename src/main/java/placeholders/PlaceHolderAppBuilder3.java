f0ffce175ce098d06cc4eb79dc108fe30e9ed959
raymondlin336 <94279301+raymondlin336@users.noreply.github.com>
2025-11-21 20:20:41 -0500

    Updated with all previous test use cases

--- src/main/java/placeholders/PlaceHolderAppBuilder3.java
+++ src/main/java/placeholders/PlaceHolderAppBuilder3.java
@@ -24,18 +24,27 @@ public class PlaceHolderAppBuilder3 {
         ArrayList<Habit> habits = new ArrayList<Habit>();
 
         Habit habit1 = new Habit(0);
-        create_and_attach_tasks(habit1, "Groceries", "Get milk, toast, eggs", Freq.Weekly, new Date(11, 2, 2025), 0, true, 5);
-        create_and_attach_tasks(habit1, "Groceries", "Get milk, toast, eggs", Freq.Weekly, new Date(11, 2, 2025), 0, false, 2);
-        create_and_attach_tasks(habit1, "Groceries", "Get milk, toast, eggs", Freq.Weekly, new Date(11, 2, 2025), 0, true, 2);
+        create_and_attach_tasks(habit1, "Groceries", "Get milk, toast, eggs", Freq.Weekly, new Date(29, 11, 2025), true, 5);
+        create_and_attach_tasks(habit1, "Groceries", "Get milk, toast, eggs", Freq.Weekly, new Date(29, 11, 2025), false, 2);
+        create_and_attach_tasks(habit1, "Groceries", "Get milk, toast, eggs", Freq.Weekly, new Date(29, 11, 2025), true, 2);
         Habit habit2 = new Habit(1);
-        create_and_attach_tasks(habit2, "Workout", "Run for 30 mins", Freq.Daily, new Date(11, 2, 2025), 0, true, 12);
-        create_and_attach_tasks(habit2, "Workout", "Run for 30 mins", Freq.Daily, new Date(11, 2, 2025), 0, false, 3);
-        create_and_attach_tasks(habit2, "Workout", "Run for 30 mins", Freq.Daily, new Date(11, 2, 2025), 0, true, 7);
-        create_and_attach_tasks(habit2, "Workout", "Run for 30 mins", Freq.Daily, new Date(11, 2, 2025), 0, false, 4);
-        create_and_attach_tasks(habit2, "Workout", "Run for 30 mins", Freq.Daily, new Date(11, 2, 2025), 0, true, 8);
+        create_and_attach_tasks(habit2, "Workout", "Run for 30 mins", Freq.Daily, new Date(4, 12, 2025), true, 33);
+        create_and_attach_tasks(habit2, "Workout", "Run for 30 mins", Freq.Daily, new Date(4, 12, 2025), false, 8);
+        create_and_attach_tasks(habit2, "Workout", "Run for 30 mins", Freq.Daily, new Date(4, 12, 2025), true, 7);
+        create_and_attach_tasks(habit2, "Workout", "Run for 30 mins", Freq.Daily, new Date(4, 12, 2025), false, 4);
+        create_and_attach_tasks(habit2, "Workout", "Run for 30 mins", Freq.Daily, new Date(4, 12, 2025), true, 28);
+        Habit habit3 = new Habit(2);
+        create_and_attach_tasks(habit3, "Basketball with friends", "Sumaid, Henry, and Kevin", Freq.Monthly, new Date(8, 12, 2025), true, 1);
+        create_and_attach_tasks(habit3, "Basketball with friends", "Sumaid, Henry, and Kevin", Freq.Monthly, new Date(8, 12, 2025), false, 1);
+        create_and_attach_tasks(habit3, "Basketball with friends", "Sumaid, Henry, and Kevin", Freq.Monthly, new Date(8, 12, 2025), true, 2);
+        Habit habit4 = new Habit(3);
+        create_and_attach_tasks(habit4, "Doctor's", "Appointment at 4pm", Freq.Once, new Date(10, 12, 2025), false, 1);
+
 
         habits.add(habit1);
         habits.add(habit2);
+        habits.add(habit3);
+        habits.add(habit4);
 
         // Editing a task
         EditTaskController editTaskController = new EditTaskController(true);
@@ -64,9 +73,9 @@ public class PlaceHolderAppBuilder3 {
         statisticsController.addScreenManager(manager);
         manager.showStatisticsView();
     }
-    public static void create_and_attach_tasks(Habit habit, String name, String desc, Freq freq, Date date, int id, Boolean completed, int total){
+    public static void create_and_attach_tasks(Habit habit, String name, String desc, Freq freq, Date date, Boolean completed, int total){
         for (int i = 0; i < total; i++) {
-            Task task = new  Task(name, desc, freq, date, id, completed);
+            Task task = new  Task(name, desc, freq, date, i, completed);
             habit.add_task(task);
         }
     }
