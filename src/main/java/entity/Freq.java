dd585b7e2996f3457de72c33120ca4964472cba6
raymondlin336 <94279301+raymondlin336@users.noreply.github.com>
2025-11-22 21:47:14 -0500

    Entity changes needed for UI

--- src/main/java/entity/Freq.java
+++ src/main/java/entity/Freq.java
@@ -1,5 +1,18 @@
 package entity;
 
 public enum Freq {
-    Once, Daily, Weekly, Monthly
+    Once, Daily, Weekly, Monthly;
+    public static String getType(Freq freq) {
+        String type = "Once";
+        if (freq.equals(Daily)) {
+            type = "Day";
+        }
+        else if (freq.equals(Weekly)) {
+            type = "Week";
+        }
+        else if  (freq.equals(Monthly)) {
+            type = "Month";
+        }
+        return type;
+    }
 }
