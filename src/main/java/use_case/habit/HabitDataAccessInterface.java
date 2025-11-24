11774d59ec81b861160e5269386ecffd5a62616d
dddd112331 <142059080+dddd112331@users.noreply.github.com>
2025-11-24 14:06:00 -0500

    add HabitDataAccessInterface.java

---
+++ src/main/java/use_case/habit/HabitDataAccessInterface.java
@@ -0,0 +1,22 @@
+package use_case.habit;
+
+import entity.Habit;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Data Access Interface for Habit entities.
+ * Defines the contract for persisting and retrieving habits.
+ */
+public interface HabitDataAccessInterface {
+    Habit save(Habit habit);
+
+    Optional<Habit> findById(int id);
+
+    List<Habit> findAll();
+
+    void deleteById(int id);
+
+    int getNextId();
+}
