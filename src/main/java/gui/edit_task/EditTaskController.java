56b9a6767ee33aa112985c28d6bf0147a47a4f88
raymondlin336 <94279301+raymondlin336@users.noreply.github.com>
2025-11-24 13:30:07 -0500

    Weekend GUI changes added

--- src/main/java/gui/edit_task/EditTaskController.java
+++ src/main/java/gui/edit_task/EditTaskController.java
@@ -1,22 +1,23 @@
 package gui.edit_task;
 
+import entity.Freq;
 import gui.task.TaskController;
-import main.Habit;
+import entity.Habit;
 
 public class EditTaskController extends TaskController {
     public EditTaskController(Boolean log_messages) {
         super(log_messages);
     }
     public void save_habit_info(int habit_id, String name, String description, String frequency){
-        Habit.Freq freq = Habit.Freq.Once;
-        if (frequency == "Every day"){
-            freq = Habit.Freq.Every_day;
+        Freq freq = Freq.Once;
+        if (frequency == "Daily"){
+            freq = Freq.Daily;
         }
-        else if (frequency == "Every week"){
-            freq = Habit.Freq.Every_day;
+        else if (frequency == "Weekly"){
+            freq = Freq.Weekly;
         }
-        else if (frequency == "Every month"){
-            freq = Habit.Freq.Every_month;
+        else if (frequency == "Monthly"){
+            freq = Freq.Monthly;
         }
         showHomeWindow();
         print_log_message("updating habit info for habit {ID: " + habit_id + " name: [" + name + "] description: [" + description + "] frequency: [" + freq + "]}");
--- src/main/java/gui/new_task/NewTaskController.java
+++ src/main/java/gui/new_task/NewTaskController.java
@@ -1,22 +1,23 @@
 package gui.new_task;
 
+import entity.Freq;
 import gui.task.TaskController;
-import main.Habit;
+import entity.Habit;
 
 public class NewTaskController extends TaskController {
     public NewTaskController(Boolean log_messages) {
         super(log_messages);
     }
     public void add_habit(String name, String description, String frequency){
-        Habit.Freq freq = Habit.Freq.Once;
-        if (frequency == "Every day"){
-            freq = Habit.Freq.Every_day;
+        Freq freq = Freq.Once;
+        if (frequency == "Daily"){
+            freq = Freq.Daily;
         }
-        else if (frequency == "Every week"){
-            freq = Habit.Freq.Every_week;
+        else if (frequency == "Weekly"){
+            freq = Freq.Weekly;
         }
-        else if (frequency == "Every month"){
-            freq = Habit.Freq.Every_month;
+        else if (frequency == "Monthly"){
+            freq = Freq.Monthly;
         }
         showHomeWindow();
         print_log_message("adding habit {ID: [unassigned] name: [" + name + "] description: [" + description + "] frequency: [" + freq + "]}");
