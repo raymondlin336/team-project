b435350e2e5a382ba864a63ad5b320c0844cd291
dddd112331 <142059080+dddd112331@users.noreply.github.com>
2025-11-24 14:22:17 -0500

    add CreateHabitInputBoundary.java and CreateHabitInputData.java

---
+++ src/main/java/use_case/habit/create/CreateHabitInputData.java
@@ -0,0 +1,37 @@
+package use_case.habit.create;
+
+import entity.Date;
+import entity.Freq;
+
+/**
+ * Input data required to create a habit.
+ */
+public class CreateHabitInputData {
+    private final String name;
+    private final String description;
+    private final Freq frequency;
+    private final Date startDate;
+
+    public CreateHabitInputData(String name, String description, Freq frequency, Date startDate) {
+        this.name = name;
+        this.description = description;
+        this.frequency = frequency;
+        this.startDate = startDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Freq getFrequency() {
+        return frequency;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+}
